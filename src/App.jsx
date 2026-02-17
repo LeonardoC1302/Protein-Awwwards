581bd7d8130fd026d84e27e463c3bc823833fd16
Leonardo Céspedes <101959981+LeonardoC1302@users.noreply.github.com>
2026-02-16 19:25:15 -0600

    [FE] Hero Section Text Reveal Animation (#2)
    * [FE] Hero Section Text Reveal Animation
    
    * [FE] Removed unnecessary div

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import gsap from "gsap";
 import NavBar from "./components/NavBar";
 import HeroSection from "./sections/HeroSection";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
